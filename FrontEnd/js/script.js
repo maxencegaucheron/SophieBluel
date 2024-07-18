00edbf7bbd6b6e4ed1b44abf28774d348e559d0d
Maxence Gaucheron <maxence.mvfg@gmail.com>
2024-07-18 23:13:52 +0200

    Récupération des catégories depuis l'API

--- FrontEnd/js/script.js
+++ FrontEnd/js/script.js
@@ -15,7 +15,7 @@ fetch("http://localhost:5678/api/works")
         console.log("Data received", data);
         const worksGallery = document.querySelector(".gallery");
         if (!worksGallery) {
-            console.error("Element with class 'gallery' not found.");
+            console.error("Element with class \"gallery\" not found.");
             return;
         }
         const worksTotal = data.length;
@@ -30,6 +30,32 @@ fetch("http://localhost:5678/api/works")
             worksGallery.appendChild(worksContainer);
             worksContainer.appendChild(worksImage);
             worksContainer.appendChild(worksCaption);
+        }
+    })
+
+    .catch(error => {
+        console.log("Erreur:", error);
+    });
+
+// Récupération des catégories
+console.log("Starting fetch operation to http://localhost:5678/api/categories");
+
+fetch("http://localhost:5678/api/categories")
+    .then(response => {
+        if (response.ok) {
+            console.log("Categories fetched properly");
+            return response.json();
+        } else {
+            console.log("Request failed:", response.status);
+            throw new Error("Network response was not ok");
+        }
+    })
+    .then(data => {
+        console.log("Data received", data);
+        const worksCategories = document.querySelector(".categories");
+        if (!worksCategories) {
+            console.error("Element with class \"categories\" not found.");
+            return;
         }
     })
 
