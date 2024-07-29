2d230489b0477dbf2b8f1abb0b40cab20c2428d5
Maxence Gaucheron <maxence.mvfg@gmail.com>
2024-07-29 20:34:11 +0200

    Séparation des scripts de la page index et login

---
+++ FrontEnd/js/script-login.js
@@ -0,0 +1,37 @@
+// Intégration du design de la page de formulaire
+localStorage.setItem("user_token", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjEsImlhdCI6MTcyMjI3NzMyMywiZXhwIjoxNzIyMzYzNzIzfQ.Ad1jzWll9tVKDMoudDEZjN1pfreWd-SKy-5npIm9nag")
+
+function addListenerAuthentification() {
+    const loginForm = document.querySelector(".login_user");
+    loginForm.addEventListener("submit", function (event) {
+        event.preventDefault();
+
+        const login = {
+            email: event.target.querySelector("[name=email]").value,
+            password: event.target.querySelector("[name=password]").value,
+        };
+
+        const failedMessage = document.getElementById("failed_login");
+        if (failedMessage) {
+            failedMessage.remove();
+        }
+        else { }
+
+        if (login.email === "sophie.bluel@test.tld" && login.password === "S0phie") {
+            console.log("Successfully authenticated");
+            window.location.href = "./index.html";
+            const token = localStorage.getItem("user_token");
+        }
+
+        else {
+            console.log("Authentication failed");
+            const failedLogin = document.createElement("p");
+            failedLogin.innerHTML = "Votre identifiant ou votre mot de passe est erronné.<br>Veuillez réessayer.";
+            failedLogin.id = "failed_login";
+            const loginForm = document.querySelector(".login_user");
+            loginForm.appendChild(failedLogin);
+        }
+    });
+}
+
+addListenerAuthentification();
